364b8e7d9b8491c0cc2d974cda59c258dd21a810
ww9 <ww9nine9@gmail.com>
2021-11-26 09:44:42 -0400

    Reproducing bug with sveltekit:prefetch in a fresh svelte-kit install

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<h1>sveltekit:prefetch test</h1>
+
+<a sveltekit:prefetch href="/">home</a>
+<a sveltekit:prefetch href="/blog">blog</a>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Home</h1>
+<p>Hovering the blog link with your mouse pointer for the first time shoould preload /blog but it isn't. See network/all tab in browser devtools.</p>
